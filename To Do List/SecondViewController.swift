283720cc7ad19a029dd62fc72d1dc5ad8194e177
aartithepanda <aartipanda@pal-nat186-18-106.itap.purdue.edu>
2016-11-03 00:20:59 -0400

    added details field

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var item: UITextField!
+    
+    @IBOutlet var date: UITextField!
+    
+    @IBOutlet var details: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        toDoList.append(item.text! + " by " + date.text!)
+        item.text = ""
+        date.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +33,16 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField:UITextField!) -> Bool {
+        item.resignFirstResponder()
+        date.resignFirstResponder()
+        return true
     }
 
 
